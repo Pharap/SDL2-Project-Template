e41bf4f5c1efd16c5e6f69f452f9d58accad3089
Pharap <2933055+Pharap@users.noreply.github.com>
2020-06-27 22:02:05 +0100

    Improve main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <SDL.h>
+#include <cstdlib>
 
-int main(int argCount, char * args[])
+int main(int argumentCount, char * arguments[])
 {
 	SDL_Window * window = SDL_CreateWindow("SDL!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 400, SDL_WindowFlags::SDL_WINDOW_ALLOW_HIGHDPI);
 
 	SDL_Delay(1000);
 
 	SDL_DestroyWindow(window);
-	return 0;
+
+	return EXIT_SUCCESS;
 }
